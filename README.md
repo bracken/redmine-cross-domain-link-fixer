8d291cbb02457955579b9b04dc32a456ba8a7440
Ryan Shaw <ryankshaw@gmail.com>
2012-09-10 15:32:39 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 redmine-cross-domain-link-fixer
 ===============================
+make redmine links to canvas domains handle all the x-domain stuff for you.
+
+To install it, just [click here](http://github.com/ryankshaw/redmine-cross-domain-link-fixer/raw/master/redmine-cross-domain-link-fixer.user.js)
---
+++ remine-cross-domain-link-fixer.user.js
@@ -0,0 +1,30 @@
+// This is a Greasemonkey user script.
+//
+// To install, you need Greasemonkey: https://addons.mozilla.org/en-US/firefox/addon/748
+// Then restart Firefox and revisit this script.
+// Under Tools, there will be a new menu item to "Install User Script".
+// Accept the default configuration and install.
+//
+// To uninstall, go to Tools/Manage User Scripts,
+// select "Hello World", and click Uninstall.
+//
+// --------------------------------------------------------------------
+//
+// ==UserScript==
+// @name          Redmine Cross Domain Link Fixer
+// @description   make redmine links to canvas domains handle all the x-domain stuff for you
+// ==/UserScript==
+
+var X_DOMAIN_STRING = 'cross_domain_login=siteadmin.instructure.com'
+
+var links = document.getElementById('content').getElementsByTagName('a');
+for (var i = 0; i < links.length; i++) {
+  var link = links[i];
+  if (link.protocol === "https:" && link.host.match(/.instructure.com$/)) {
+    if (link.search) {
+      link.search = link.search + '&' + X_DOMAIN_STRING
+    } else {
+      link.search = X_DOMAIN_STRING
+    }
+  }
+}
